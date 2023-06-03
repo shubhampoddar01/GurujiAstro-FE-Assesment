c32763d14e18c9f0cd1bf80d615e6e0289fd6841
shubhampoddar01 <shubham.poddar0105@gmail.com>
2023-06-03 20:08:59 +0530

    Updates

--- src/components/index.jsx
+++
@@ -1,7 +0,0 @@
-import Header from "./header";
-import Testimonial from "./user testimonial";
-import Chackra from "./Chackra";
-import UserReview from "./UserReview";
-import Footer from "./Footer";
-
-export { Header, Testimonial, Chackra, UserReview, Footer };
